b70581ff358d311009bd17d4ebbd9cb7d5bc6a14
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-06-21 01:21:46 +0530

    start

---
+++ demo/src/test/java/com/example/demo/SpCuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
