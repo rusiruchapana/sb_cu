bea88e2ea5cc2048c5da14c8c45520fd911d4b5c
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-06-21 08:11:21 +0530

    create the entity class.

---
+++ demo/src/main/java/com/example/demo/model/Task.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 100, message = "Title must be less than 100 characters")
+    private String title;
+
+    @Size(max = 500, message = "Description must be less than 500 characters")
+    private String description;
+
+    private boolean completed;
+}
