704b2e8d263c479e94353938d2055b8ab2851571
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-06-21 09:01:12 +0530

    update

--- demo/src/main/java/com/example/demo/service/TaskService.java
+++ demo/src/main/java/com/example/demo/service/TaskService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import com.example.demo.model.Task;
 import com.example.demo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
